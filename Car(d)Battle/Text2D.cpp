3f7c2d1170090d07b85de76990ab7738c2fdf234
Łukasz Lesiecki <theluki525@o2.pl>
2021-04-16 23:43:15 +0200

    Improved implementation of Text2D class.

--- Car(d)Battle/Text2D.cpp
+++ Car(d)Battle/Text2D.cpp
@@ -1,1 +1,8 @@
 #include "Text2D.h"
+
+void Text2D::render()
+{
+	glRasterPos3f(x, y, 1.0f);
+	for (char ch : text)
+		glutBitmapCharacter(font, ch);
+}
--- Car(d)Battle/Text2D.h
+++ Car(d)Battle/Text2D.h
@@ -8,10 +8,5 @@ struct Text2D
 	std::string text;
 	void* font;//eg: GLUT_BITMAP_HELVETICA_18, GLUT_BITMAP_TIMES_ROMAN_24
 
-	void render()
-	{
-		glRasterPos3f(x, y, 1.0f);
-		for (char ch : text)
-			glutBitmapCharacter(font, ch);
-	}
+	void render();
 };
