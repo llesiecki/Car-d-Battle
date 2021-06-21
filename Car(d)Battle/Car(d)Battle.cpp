074f931904c4161874d653ac43fece77e232718a
Łukasz Lesiecki <theluki525@o2.pl>
2021-06-21 19:26:43 +0200

    Extended the GLFW test in main()

--- Car(d)Battle/Car(d)Battle.cpp
+++ Car(d)Battle/Car(d)Battle.cpp
@@ -1,12 +1,45 @@
 #include "stdafx.h"
-//#include "Game.h"
+#include "Game.h"
 
 int main(int argc, char* argv[])
 {
     //GLFW test
     if(glfwInit() != GLFW_TRUE)
         exit(1);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, GAME_NAME, NULL, NULL);
+    if (window == NULL)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return 2;
+    }
+
+    glfwMakeContextCurrent(window);
 	gladLoadGL();
+
+    glViewport(0, 0, 1280, 720);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CCW);
+    glLineWidth(1.5);
+
+    static Game game;//See Note 1
+    
+    glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int w, int h) {return game.set_screen_size(w, h); });
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     glfwTerminate();
 
     //Commented out due to refactor with GLFW
