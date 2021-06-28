40c4fd30ae6eab9b53edae9f424aac3b534cd957
Łukasz Lesiecki <theluki525@o2.pl>
2021-06-28 20:56:55 +0200

    Fixed settings of reading the VBO in the Scene class.

--- Car(d)Battle/Scene.cpp
+++ Car(d)Battle/Scene.cpp
@@ -77,12 +77,12 @@ void Scene::load()
 	glEnableVertexAttribArray(0);
 
 	//attrib 1 - tex coords
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), static_cast<void*>(0));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), reinterpret_cast<void*>(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
-	
+
 	background->Load();
 }
