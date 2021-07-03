3672475de08ea1bc963000178708dc8d396c257a
Łukasz Lesiecki <theluki525@o2.pl>
2021-07-03 22:39:06 +0200

    Updated the Texture utility to use the modern OpenGL syntax.

--- Car(d)Battle/utilities/Texture.cpp
+++ Car(d)Battle/utilities/Texture.cpp
@@ -31,6 +31,7 @@ bool CTexture::Load(void)
 			printf("ERROR: Cannot read texture file \"%s\".\n", _file.c_str());
 			return false;
 		}
+		glEnable(GL_TEXTURE_2D);
 
 		glGenTextures(1, &_id);
 
@@ -39,12 +40,8 @@ bool CTexture::Load(void)
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, _magFilter);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, _minFilter);
 
-		if (_minFilter == GL_LINEAR_MIPMAP_LINEAR || _minFilter == GL_LINEAR_MIPMAP_NEAREST) {
-			gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, tex->width, tex->height, GL_RGB, GL_UNSIGNED_BYTE, tex->data);
-		}
-		else {
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex->width, tex->height, 0, GL_RGB, GL_UNSIGNED_BYTE, tex->data);
-		}
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex->width, tex->height, 0, GL_RGB, GL_UNSIGNED_BYTE, tex->data);
+		glGenerateMipmap(GL_TEXTURE_2D);
 
 		delete tex;
 
@@ -76,12 +73,9 @@ bool CTexture::Load(void)
 		for (unsigned int i = 0; i < height/2; i++)
 			for (unsigned int j = 0; j < width*4; j++)
 				swap(image[i*width*4 + j], image[(height - i-1) * width * 4 + j]);
-		if (_minFilter == GL_LINEAR_MIPMAP_LINEAR || _minFilter == GL_LINEAR_MIPMAP_NEAREST) {
-			gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, width, height, GL_RGBA, GL_UNSIGNED_BYTE, image.data());
-		}
-		else {
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image.data());
-		}
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image.data());
+		glGenerateMipmap(GL_TEXTURE_2D);
 
 		IsLoaded = true;
 
