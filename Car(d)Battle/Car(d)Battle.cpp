e91730a876300fdd9da523df1dcfcd08e8d76eaa
Łukasz Lesiecki <theluki525@o2.pl>
2021-07-09 22:43:41 +0200

    Replaced the old glut-based render code with the new GLFW-based

--- Car(d)Battle/Car(d)Battle.cpp
+++ Car(d)Battle/Car(d)Battle.cpp
@@ -3,86 +3,58 @@
 
 int main(int argc, char* argv[])
 {
-    //GLFW test
-    if(glfwInit() != GLFW_TRUE)
-        exit(1);
+	//GLFW test
+	if(glfwInit() != GLFW_TRUE)
+		exit(1);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(1280, 720, GAME_NAME, NULL, NULL);
-    if (window == NULL)
-    {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return 2;
-    }
+	GLFWwindow* window = glfwCreateWindow(1280, 720, GAME_NAME, NULL, NULL);
+	if (window == NULL)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return 2;
+	}
 
-    glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window);
 	gladLoadGL();
 
-    glViewport(0, 0, 1280, 720);
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glFrontFace(GL_CCW);
-    glLineWidth(1.5);
+	glViewport(0, 0, 1280, 720);
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glFrontFace(GL_CCW);
+	glLineWidth(1.5);
 
-    static Game & game = Singleton<Game>();//See Note 1
-    
-    glfwSetFramebufferSizeCallback(window,
-        [](GLFWwindow* window, int w, int h)
-        {return game.set_screen_size(w, h);
-        });
-    glfwSwapInterval(1);//V-SYNC
+	static Game & game = Singleton<Game>();//See Note 1
+	
+	glfwSetFramebufferSizeCallback(window,
+		[](GLFWwindow* window, int w, int h)
+		{return game.set_screen_size(w, h);
+		});
+	glfwSetCursorPosCallback(window,
+		[](GLFWwindow* window, double x, double y)
+		{return game.set_cursor_pos(x, y);
+		});
+	glfwSwapInterval(1);//V-SYNC
 
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		game.draw();
+		glfwSwapBuffers(window);
+	}
 
-    glfwTerminate();
-
-    //Commented out due to refactor with GLFW
-    /*
-	glutInit(&argc, argv);
-	glutInitWindowSize(1280, 720);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutCreateWindow(GAME_NAME);
-    static Game game;//See Note 1
-    game.load();
-
-    unsigned int opp_num;
-    do
-    {
-        std::cout << "Number of opponents for the next round: ";
-        //std::cin >> opp_num;
-        opp_num = 3;
-    } 		while (opp_num < 1 && opp_num > 3);
-    game.start(opp_num + 1);
-
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glFrontFace(GL_CCW);
-    glLineWidth(1.5);
-
-    static void (*render_timer)(int) = [](int place_holder){//See Note 1
-        game.draw();
-        glutTimerFunc(17, render_timer, 0); };
-
-	glutTimerFunc(17, render_timer, 0);
-	glutDisplayFunc([]() {return game.draw(); });
-    glutReshapeFunc([](int w, int h) {return game.set_screen_size(w, h); });
-    glutPassiveMotionFunc([](int x, int y) {return game.set_cursor_pos(x, y); });
-	glutMainLoop();
-    */
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
 
 /// Note 1:
 /// Must be static as it must be used in lambdas without capture lists.
-/// Capture lists cannot be used in these lambdas, because they are passed as C-style function pointers.
+/// Capture lists cannot be used in these lambdas,
+/// because they are passed as C-style function pointers.
