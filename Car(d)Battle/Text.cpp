51726a72ba56b498abb573a926cb10c63d8832d4
Łukasz Lesiecki <theluki525@o2.pl>
2021-08-17 21:14:58 +0200

    Implemented the color-related member functions of the Text class

--- Car(d)Battle/Text.cpp
+++ Car(d)Battle/Text.cpp
@@ -1,1 +1,58 @@
 #include "Text.h"
+
+Text::Text()
+{
+	set_color(glm::vec4(255, 255, 255, 255));
+	VAO = 0;
+	VBO = 0;
+}
+
+Text::Text(const std::string& text)
+{
+	this->Text::Text();
+	set_text(text);
+}
+
+Text::Text(const std::string& text, glm::vec4 color)
+{
+	this->Text::Text(text);
+	set_color(color);
+}
+
+Text::~Text()
+{
+	if (VAO)
+		glDeleteVertexArrays(1, &VAO);
+
+	if (VBO)
+		glDeleteBuffers(1, &VBO);
+}
+
+void Text::draw()
+{
+}
+
+void Text::set_text(const std::string& text)
+{
+	this->text = text;
+}
+
+void Text::set_color(glm::vec4 color)
+{
+	this->color = color;
+}
+
+void Text::set_color(glm::vec3 color)
+{
+	set_color(glm::vec4(color, this->color.w));
+}
+
+void Text::set_color(glm::vec3 color, GLfloat opacity)
+{
+	set_color(glm::vec4(color, opacity));
+}
+
+void Text::set_opacity(GLfloat opacity)
+{
+	this->color.w = opacity;
+}
--- Car(d)Battle/Text.h
+++ Car(d)Battle/Text.h
@@ -5,13 +5,19 @@
 class Text
 {
 	std::string text;
-	GLuint VBO, VBO;
+	GLuint VBO, VAO;
+	glm::vec4 color;
 
 public:
 	Text();
 	Text(const std::string&);
+	Text(const std::string&, glm::vec4);
 	~Text();
 
 	void draw();
 	void set_text(const std::string&);
+	void set_color(glm::vec4);
+	void set_color(glm::vec3);
+	void set_color(glm::vec3, GLfloat);
+	void set_opacity(GLfloat);
 };
