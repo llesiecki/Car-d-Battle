d24b74a572663b8daa65c57c6edf7452d2c8efd0
Łukasz Lesiecki <theluki525@o2.pl>
2021-09-07 01:45:19 +0200

    Fixed and improved Line class code

--- Car(d)Battle/Line.cpp
+++ Car(d)Battle/Line.cpp
@@ -1,93 +1,86 @@
 #include "Line.h"
 
 Line::Line()
-    :mvp(), color(), start(), end()
+	:mvp(), color(), start(), end()
 {
-    shader.load("shaders/line_vert.glsl", GL_VERTEX_SHADER);
-    shader.load("shaders/line_frag.glsl", GL_FRAGMENT_SHADER);
-    shader.link();
+	shader.load("shaders/line_vert.glsl", GL_VERTEX_SHADER);
+	shader.load("shaders/line_frag.glsl", GL_FRAGMENT_SHADER);
 
-    glGenVertexArrays(1, &vao);
-    glGenBuffers(1, &vbo);
+	shader.link();
+
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
 }
 
 Line::Line(const glm::vec3& start, const glm::vec3& end)
 {
-    this->Line::Line();
-    this->start = start;
-    this->end = end;
-
-    float vertices[] = {
-         start.x, start.y, start.z,
-         end.x, end.y, end.z
-    };
-
-    glBindVertexArray(vao);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(0));
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+	this->Line::Line();
+	set_pos(start, end);
 }
 
 Line::Line(const glm::vec3& start, const glm::vec3& end, const glm::vec3& color)
 {
-    this->Line::Line(start, end);
-    this->color = color;
+	this->Line::Line(start, end);
+	this->color = color;
 }
 
 void Line::clean_buffers()
 {
-    if (vao != 0)
-        glDeleteVertexArrays(1, &vao);
+	if (vbo != 0)
+		glDeleteBuffers(1, &vbo);
 
-    if (vbo != 0)
-        glDeleteVertexArrays(1, &vbo);
+	if (vao != 0)
+		glDeleteVertexArrays(1, &vao);
 }
 
 Line::~Line()
 {
-    clean_buffers();
+	clean_buffers();
 }
 
 void Line::set_MVP(const glm::mat4& mvp)
 {
-    this->mvp = mvp;
+	this->mvp = mvp;
 }
 
 void Line::set_color(const glm::vec3& color)
 {
-    this->color = color;
+	this->color = color;
 }
 
 void Line::set_pos(const glm::vec3& start, const glm::vec3& end)
 {
-    clean_buffers();
-    this->Line::Line(start, end);
+	this->start = start;
+	this->end = end;
+
+	float vertices[] = {
+		 start.x, start.y, start.z,
+		 end.x, end.y, end.z
+	};
+
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 }
 
 void Line::set_start(const glm::vec3& start)
 {
-    set_pos(start, end);
+	set_pos(start, end);
 }
 
 void Line::set_end(const glm::vec3& end)
 {
-    set_pos(start, end);
+	set_pos(start, end);
 }
 
 void Line::draw()
 {
-    shader.enable();
-    shader.set("MVP", mvp);
-    shader.set("color", color);
+	shader.enable();
+	shader.set("MVP", mvp);
+	shader.set("color", color);
 
-    glBindVertexArray(vao);
-    glDrawArrays(GL_LINES, 0, 2);
-
-    glBindVertexArray(0);
-    shader.disable();
+	glBindVertexArray(vao);
+	glDrawArrays(GL_LINES, 0, 2);
 }
