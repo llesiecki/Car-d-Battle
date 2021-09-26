d606d7ab668650d99c6bbf795a7a40aebdc390da
Łukasz Lesiecki <theluki525@o2.pl>
2021-09-27 00:37:25 +0200

    Created a definition of the Button class and its members

---
+++ Car(d)Battle/Button.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "stdafx.h"
+#include "Text.h"
+#include "Shader.h"
+#include "utilities/Texture.h"
+
+class Button
+{
+	bool pressed;
+	bool highlight;
+	GLuint vao;
+	GLuint ebo;
+	GLuint vbo;
+	CTexture * texture;
+	glm::ivec2 pos;
+	glm::ivec2 size;
+	glm::mat4 m, p, local, transform;
+	Text text;
+	Shader shader;
+
+	void racalculate_transform();
+
+public:
+	Button();
+	~Button();
+	void draw() const;
+	bool is_hovered(const glm::ivec2&) const;
+	void set_projection(const glm::mat4&);
+	void set_highlight(bool);
+	void set_press(bool);
+	void set_pos(const glm::ivec2&);
+	void set_size(const glm::ivec2&);
+	void set_text(const std::string&);
+	void set_texture(const std::string&);
+};
