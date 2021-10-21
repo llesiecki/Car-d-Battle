6f72c69d5200728027b6eb1fc8336b884f5ef5fa
Łukasz Lesiecki <theluki525@o2.pl>
2021-10-21 21:59:30 +0200

    Implemented a vertex shader for the Blur class

--- Car(d)Battle/shaders/blur_vert.glsl
+++ Car(d)Battle/shaders/blur_vert.glsl
@@ -1,1 +1,11 @@
 #version 330 core
+layout (location = 0) in vec2 verts;
+layout (location = 1) in vec2 tex_coords;
+
+out vec2 _tex_coords;
+
+void main()
+{
+    _tex_coords = tex_coords;
+    gl_Position = vec4(verts, 0.0, 1.0);
+}
