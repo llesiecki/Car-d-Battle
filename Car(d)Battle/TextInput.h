456c479c024896b8252ddb493716c489b1245043
Łukasz Lesiecki <theluki525@o2.pl>
2021-10-27 22:15:45 +0200

    Declared a TextInput class and its functionality

---
+++ Car(d)Battle/TextInput.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "stdafx.h"
+#include "utilities/Texture.h"
+#include "Keyboard.h"
+#include "Text.h"
+#include "Shader.h"
+
+class TextInput
+{
+	GLuint vao;
+	GLuint ebo;
+	GLuint vbo;
+	Texture* texture;
+	Keyboard* kb;
+	std::pair<float, float>* cursor_pos;
+	glm::ivec2 pos;
+	glm::ivec2 size;
+	glm::vec2 scale;
+	glm::vec2 scaled_size;
+	glm::mat4 proj, translate, transform;
+	std::function<void(const std::string&)> enter_function;
+	std::string content;
+	Text text;
+	Shader shader;
+
+	void racalculate_transform();
+
+public:
+	TextInput();
+	~TextInput();
+	void draw();
+	bool is_hovered(const glm::ivec2&) const;
+	void set_projection(const glm::mat4&);
+	void set_pos(const glm::ivec2&);
+	void set_size(const glm::ivec2&);
+	void set_scale(const glm::vec2&);
+	void set_texture(const std::string&);
+	void set_text(const std::string&);
+	void set_font(const std::string&);
+	void set_enter_function(std::function<void(const std::string&)>);
+	void set_keyboard(Keyboard*);
+	void set_cursor_pointer(std::pair<float, float>*);
+	void keyboard_callback(BYTE, Keyboard::Key_action);
+};
