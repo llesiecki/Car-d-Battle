b6a7c1333dcf30c432063427646cd8a761ab6dc0
Łukasz Lesiecki <theluki525@o2.pl>
2021-10-31 15:01:45 +0100

    Initial implementation of the TextInput class

--- Car(d)Battle/TextInput.cpp
+++ Car(d)Battle/TextInput.cpp
@@ -110,6 +110,123 @@ void TextInput::caret_function()
 	}
 }
 
+void TextInput::racalculate_transform()
+{
+	scaled_size = glm::ivec2(size.x * scale.x, size.y * scale.y);
+
+	transform =
+		proj
+		* translate
+		* glm::scale(glm::mat4(1.0f), { scaled_size.x, scaled_size.y, 1 });
+
+	float fill_y = (scaled_size.y * 0.8f) / FONT_SIZE;
+
+	glm::mat4 text_scale = glm::scale(glm::mat4(1.0f), {
+		fill_y,
+		fill_y,
+		1 });
+
+	glm::mat4 text_center = glm::translate(glm::mat4(1.0f), {
+		0,
+		(scaled_size.y - FONT_SIZE * fill_y) / 2 + text.get_descent() * fill_y,
+		0 });
+
+	text.set_mvp(proj * translate * text_center * text_scale);
+}
+
+void TextInput::draw()
+{
+	glDisable(GL_DEPTH_TEST);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture->get_id());
+	shader.enable();
+	shader.set("tex_id", 0);
+	shader.set("transform", transform);
+	glBindVertexArray(vao);
+
+	glDrawElements(GL_TRIANGLES, 2 * 3, GL_UNSIGNED_BYTE, static_cast<void*>(0));
+
+	text.draw();
+	glEnable(GL_DEPTH_TEST);
+}
+
+bool TextInput::is_hovered(const glm::ivec2& cursor) const
+{
+	return
+		cursor.x >= pos.x &&
+		cursor.x < pos.x + scaled_size.x &&
+		cursor.y > pos.y &&
+		cursor.y <= pos.y + scaled_size.y;
+}
+
+void TextInput::set_projection(const glm::mat4& projection)
+{
+	proj = projection;
+	racalculate_transform();
+}
+
+void TextInput::set_pos(const glm::ivec2& pos)
+{
+	this->pos = pos;
+	translate = glm::translate(glm::mat4(1.0f), { pos.x, pos.y, 0 });
+	racalculate_transform();
+}
+
+void TextInput::set_size(const glm::ivec2& size)
+{
+	this->size = size;
+	racalculate_transform();
+}
+
+void TextInput::set_scale(const glm::vec2& scale)
+{
+	this->scale = scale;
+	racalculate_transform();
+}
+
+void TextInput::set_text(const std::string& text)
+{
+	this->text.set_text(text);
+	racalculate_transform();
+}
+
+void TextInput::set_font(const std::string& font)
+{
+	this->text.set_font(font);
+	racalculate_transform();
+}
+
+void TextInput::set_id(const std::string& id)
+{
+	this->id = id;
+}
+
+void TextInput::set_enter_function(std::function<void(const std::string&)> function)
+{
+	enter_function = function;
+}
+
+void TextInput::set_keyboard(Keyboard* keyboard)
+{
+	kb = keyboard;
+	if (kb)
+	{
+		std::function<void(BYTE, Keyboard::Key_action)> fp =
+			std::bind(&TextInput::keyboard_callback, this, std::_Ph<1>(), std::_Ph<2>());
+		kb->observe_key(VK_LBUTTON, fp);
+	}
+}
+
+std::string TextInput::get_text()
+{
+	return content;
+}
+
+void TextInput::set_cursor_pointer(std::pair<float, float>* cursor_pos)
+{
+	this->cursor_pos = cursor_pos;
+}
+
 void TextInput::keyboard_callback(BYTE key, Keyboard::Key_action act)
 {
 	if (key == VK_LBUTTON)
@@ -136,14 +253,32 @@ void TextInput::keyboard_callback(BYTE key, Keyboard::Key_action act)
 
 		if (key == VK_BACK && act == Keyboard::Key_action::on_press)
 		{
-			content.pop_back();
+			if (!content.empty())
+				content.pop_back();
 			last_input = std::chrono::system_clock::now();
 		}
 
 		if (isalnum(key) && act == Keyboard::Key_action::on_press)
 		{
 			content += key;
+			text.set_text(content);
+
+			if (text.get_width() > size.x)
+			{
+				content.pop_back();
+				text.set_text(content);
+			}
+
 			last_input = std::chrono::system_clock::now();
 		}
 	}
+}
+
+void TextInput::set_texture(const std::string& path)
+{
+	if (texture)
+		delete texture;
+	texture = new Texture(path);
+	texture->load();
+	texture->bind();
 }
