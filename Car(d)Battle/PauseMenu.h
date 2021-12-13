b5553cffc58d1fcc86c8b56f6c386dd651703f2e
Łukasz Lesiecki <theluki525@o2.pl>
2021-12-14 00:31:43 +0100

    Added a declaration of PauseMenu class, the pause menu will be moved to it

---
+++ Car(d)Battle/PauseMenu.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "stdafx.h"
+#include "UI_Interface.h"
+#include "Button.h"
+#include "Text.h"
+#include "TextInput.h"
+#include "Blur.h"
+#include "Dimmer.h"
+#include <thread>
+
+class PauseMenu
+{
+	UI_Interface* ui;
+	glm::ivec2 screen_size;
+	const glm::vec2 ref_size;
+	const glm::vec2 ref_resolution;
+	glm::mat4 mvp;
+	std::map<std::string, std::unique_ptr<Button>> buttons;
+	std::map<std::string, std::unique_ptr<Text>> texts;
+	std::map<std::string, std::unique_ptr<TextInput>> inputs;
+	Blur blur;
+	Dimmer dimmer;
+
+	void configure(std::unique_ptr<Button>&);
+	void configure(std::unique_ptr<Text>&);
+	void configure(std::unique_ptr<TextInput>&);
+
+public:
+	PauseMenu();
+	~PauseMenu();
+	void draw();
+	void set_ui(UI_Interface*);
+	void set_screen_size(glm::ivec2);
+	void set_cursor_pos(std::pair<float, float>);
+	void button_callback(const std::string&);
+	void keyboard_callback(BYTE, Keyboard::Key_action);
+};
