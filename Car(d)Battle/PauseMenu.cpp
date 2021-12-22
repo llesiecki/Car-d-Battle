d0b51b1c1ac6367bfd12cd283f096a38806f9969
Łukasz Lesiecki <theluki525@o2.pl>
2021-12-22 23:57:12 +0100

    Implemented drawing and scaling of the PauseMenu class

--- Car(d)Battle/PauseMenu.cpp
+++ Car(d)Battle/PauseMenu.cpp
@@ -56,6 +56,8 @@ void PauseMenu::configure(std::unique_ptr<Text>& text)
 
 void PauseMenu::draw()
 {
+	glDisable(GL_DEPTH_TEST);
+
 	blur.draw();
 	dimmer.draw();
 
@@ -64,6 +66,8 @@ void PauseMenu::draw()
 
 	for (auto& kv : texts)
 		kv.second->draw();
+
+	glEnable(GL_DEPTH_TEST);
 }
 
 void PauseMenu::set_ui(UI_Interface* ui)
@@ -73,7 +77,63 @@ void PauseMenu::set_ui(UI_Interface* ui)
 
 void PauseMenu::set_screen_size(const glm::ivec2 size)
 {
+	const glm::vec2 ref_size(640, 480);
+	const glm::vec2 ref_resolution(1280, 720);
 
+	// case 1 - increase the menu size on larger screens:
+	glm::vec2 scale(glm::vec2(screen_size.x, screen_size.y) / ref_resolution);
+	scale = glm::vec2(std::min(scale.x, scale.y), std::min(scale.x, scale.y));
+	glm::vec2 menu_size = ref_size * scale;
+
+	// case 2 - don't make the menu size too small on smaller screens:
+	if (menu_size.x < ref_size.x || menu_size.y < ref_size.y)
+	{
+		menu_size = ref_size;
+	}
+
+	// case 3 - match the menu size with a really small resolution:
+	if (menu_size.x > screen_size.x)
+	{
+		menu_size.x = static_cast<float>(screen_size.x);
+		menu_size.y = menu_size.x * ref_size.y / ref_size.x;
+	}
+
+	if (menu_size.y > screen_size.y)
+	{
+		menu_size.y = static_cast<float>(screen_size.y);
+		menu_size.x = menu_size.y * ref_size.x / ref_size.y;
+	}
+
+	// recalculate scale:
+	scale = menu_size / ref_size;
+	const glm::mat4 scale_mat = glm::scale(glm::mat4(1.0f), glm::vec3(scale, 1));
+
+	// calculate origin:
+	const glm::vec2 origin(
+		(screen_size.x - menu_size.x) / 2,
+		(screen_size.y - menu_size.y) / 2
+	);
+	const glm::mat4 translate_mat = glm::translate(glm::mat4(1.0f), glm::vec3(origin, 0));
+
+	// create projection matrix:
+	const glm::mat4 menu_ortho = glm::ortho(
+		0.0f, static_cast<float>(screen_size.x),
+		0.0f, static_cast<float>(screen_size.y)
+	) * translate_mat * scale_mat;
+
+	for (auto& kv : buttons)
+	{
+		kv.second->set_projection(menu_ortho);
+		kv.second->set_screen_size(screen_size);
+	}
+
+	for (auto& kv : texts)
+	{
+		kv.second->set_mvp(menu_ortho);
+	}
+
+	dimmer.set_mvp(menu_ortho);
+	dimmer.set_size(ref_size);
 }
 
 void PauseMenu::set_cursor_pos(std::pair<float, float> cursor_pos)
